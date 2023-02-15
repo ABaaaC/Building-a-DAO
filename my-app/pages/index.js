1e9adc5048149a3dd1fc552abdcc6f2cebbeee32
Stanislav <rus.abaaac@gmail.com>
2023-02-15 16:35:51 +0600

    added block interaction functions

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -10,17 +10,24 @@ export default function Home() {
 
   const [walletConnected, setWalletConnected] = useState(false);
   const [isOwner, setIsOwner] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [treasuryBalance, setTreasuryBalance] = useState("0");
+  const [numProposals, setNumProposals] = useState("0");
+  const [proposals, setProposals] = useState([]);
+  const [nftBalance, setNftBalance] = useState(0);
+
+  const [fakeNftTokenId, setFakeNftTokenId] = useState("");
+
 
   const web3ModalRef = useRef();
 
-  const getOwner = async () => {
+  /**
+   * getOwner: gets the contract owner by connected address
+   */
+  const getDAOOwner = async () => {
     try {
-      const signer = await getProviderOrSigner();
-      const daoContract = new ethers.Contract(
-        DAO_CONTRACT_ADDRESS,
-        DAO_CONTRACT_ABI,
-        signer
-      );
+      const signer = await getProviderOrSigner(); //true?
+      const daoContract = getDaoContractInstance(signer);
 
       const owner = await daoContract.owner();
       const userAddress = await signer.getAddress();
@@ -32,6 +39,194 @@ export default function Home() {
       console.error(error);
     }
   }
+
+  /**
+   * withdrawDAOEther: withdraws ether by calling
+   * the withdraw function in the contract
+   */
+  const withdrawDAOEther = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const daoContract = await getDaoContractInstance(signer);
+      
+      const tx = await daoContract.withdrawEther();
+      setLoading(true);
+      await tx.wait();
+      setLoading(false);
+      getDAOTreasuryBalance();
+
+    } catch (error) {
+      console.error(error);
+      window.alert(err.reason);
+    }
+  }
+
+  // getDAOTreasuryBalance Reads the ETH balance of the DAO contract and sets the `treasuryBalance` state variable
+  const getDAOTreasuryBalance = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      
+      const balance = await provider.getBalance(DAO_CONTRACT_ADDRESS);
+      setTreasuryBalance(balance.toString());
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // getNumProposalsInDAO Reads the number of proposals in the DAO contract and sets the `numProposals` state variable
+  const getNumProposalsInDAO = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const daoContract = await getDaoContractInstance(provider);
+
+      const daoNumProposals = await daoContract.numProposals();
+      setTreasuryBalance(daoNumProposals.toString());
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+ 
+  // getUserNFTBalance Reads the balance of the user's CryptoDevs NFTs and sets the `nftBalance` state variable
+  const getUserNFTBalance = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const nftContract = await getCryptodevsNFTContractInstance(provider);
+      
+      const balance = await nftContract.balanceOf(provider.getAddress());
+      setNftBalance(parseInt(balance.toString()));
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // createProposal Calls the `createProposal` function in the contract, using the tokenId from `fakeNftTokenId`
+const createProposal = async () => {
+  try {
+    const signer = await getProviderOrSigner(true);
+    const daoContract = await getDaoContractInstance(signer);
+    
+    const tx = await daoContract.createProposal(fakeNftTokenId);
+    setLoading(true);
+    await tx.wait();
+    await getNumProposalsInDAO();
+    setLoading(false);
+
+  } catch (error) {
+    console.error(error);
+    window.alert(err.reason);
+  }
+}
+  // fetchProposalById is Helper function to fetch and parse one proposal from the DAO contract
+  // Given the Proposal ID
+  // and converts the returned data into a Javascript object with values we can use
+  const fetchProposalById = async (id) => {
+    try {
+      const provider = await getProviderOrSigner();
+      const daoContract = await getDaoContractInstance(provider);
+
+      const proposal = await daoContract.proposals(id);
+      const parsedProposal = {
+        proposalId: id,
+        nftTokenId: proposal.nftTokenId.toString(),
+        deadline: new Date(parseInt(proposal.deadline.toString()) * 1000),
+        yayVotes: proposal.yayVotes.toString(),
+        nayVotes: proposal.nayVotes.toString(),
+        executed: proposal.executed,
+      };
+      return parsedProposal;
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+
+  // fetchAllProposals Runs a loop `numProposals` times to fetch all proposals in the DAO
+  // and sets the `proposals` state variable
+  const fetchAllProposals = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const daoContract = await getDaoContractInstance(provider);
+
+      const proposals = [];
+      for (let i = 0; i < numProposals; i++) {
+        const proposal = await fetchProposalById(i);
+        proposals.push(proposal);
+      }
+      setProposals(proposals);
+      return proposals;
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+
+  // voteOnProposal Calls the `voteOnProposal` function in the contract, using the passed
+  // proposal ID and Vote
+  const voteOnProposal = async (proposalId, _vote) => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const daoContract = await getDaoContractInstance(signer);
+      
+      let vote = _vote === "YAY" ? 0 : 1;
+      const tx = await daoContract.voteOnProposal(proposalId, vote);
+      setLoading(true);
+      await tx.wait();
+      setLoading(false);
+      await fetchAllProposals();
+  
+    } catch (error) {
+      console.error(error);
+      window.alert(err.reason);
+    }
+  } 
+
+
+  // executeProposal Calls the `executeProposal` function in the contract, using
+  // the passed proposal ID
+  const executeProposal = async (proposalId, ) => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const daoContract = await getDaoContractInstance(signer);
+      
+      const tx = await daoContract.executeProposal(proposalId, );
+      setLoading(true);
+      await tx.wait();
+      setLoading(false);
+      await fetchAllProposals();
+      getDAOTreasuryBalance();
+  
+    } catch (error) {
+      console.error(error);
+      window.alert(err.reason);
+    }
+  } 
+
+  // getDaoContractInstance is Helper function to return a DAO Contract instance
+  // given a Provider/Signer
+  const getDaoContractInstance = async (providerOrSigner) => {
+    return new ethers.Contract(
+      DAO_CONTRACT_ADDRESS,
+      DAO_CONTRACT_ABI,
+      providerOrSigner
+    );
+  }
+  
+
+  // getCryptodevsNFTContractInstance is Helper function to return a CryptoDevs NFT Contract instance
+  // given a Provider/Signer
+  const getCryptodevsNFTContractInstance = async (providerOrSigner) => {
+    return new ethers.Contract(
+      NFT_CONTRACT_ADDRESS,
+      NFT_CONTRACT_ABI,
+      providerOrSigner
+    );
+  }
+
 
   const getProviderOrSigner = async (needSigner = false) => {
     const provider = await web3ModalRef.current.connect();
@@ -66,10 +261,22 @@ export default function Home() {
         disableInjectedProvider: false
       })
     } else {
-      getOwner();
+      getDAOOwner();
+      getNumProposalsInDAO();
+      getDAOTreasuryBalance();
+      getUserNFTBalance();
     }
 
   }, [walletConnected]);
+
+  // useEffect Piece of code that runs everytime the value of `selectedTab` changes
+  // Used to re-fetch all proposals in the DAO when user switches
+  // to the 'View Proposals' tab
+  useEffect(() => {
+    if (selectedTab === "View Proposals") {
+      fetchAllProposals();
+    }
+  }, [selectedTab])
 
   function renderButton() {
     if (!walletConnected) {
@@ -82,6 +289,15 @@ export default function Home() {
       )
     }
   }
+
+
+  // renderTabs is Render the contents of the appropriate tab based on `selectedTab`
+
+
+  // renderCreateProposalTab Renders the 'Create Proposal' tab content
+
+
+  // renderViewProposalsTab Renders the 'View Proposals' tab content
 
   return (
     <div>
